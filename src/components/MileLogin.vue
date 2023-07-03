4223f184bba0735d526279e7f29f9f5a6b8d1cdb
Mile99231 <751550475@qq.com>
2023-07-03 15:17:00 +0800

    更新了MileLogin登录界面

---
+++ src/components/MileLogin.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="ad">
+        <div class="background">
+            <img src="../assets/e90664acbb424877adfaa40ca21cd35b.jpeg" width="100%" height="100%">
+        </div>
+        <div class="login">
+            <el-input v-model="username" placeholder="请输入内容" class="pass" style="margin-top: 350px;margin-left: 30px;"></el-input><br>
+            <el-input placeholder="请输入密码" v-model="password" show-password class="user" style="margin-top: 20px; margin-left: 30px;"></el-input><br>
+            <el-button type="primary" style="margin-top: 30px;margin-left: 60px;" @click="login()">登录</el-button>
+            <el-button type="warning" style="margin-left: 70px;">注册</el-button>
+        </div>
+        
+    </div>    
+    </template>
+    
+    <script>
+     export default{
+        name :'MileLogin',
+        data(){
+            return{
+                username: '',
+                password:''
+            }
+        },
+        methods:{
+            login(){
+                this.$message({
+          message: '登录成功',
+          type: 'success'
+        });
+            },
+            reg(){
+                this.$message({
+          message: '注册成功',
+          type: 'success'
+        });
+            }
+            
+        },
+        created(){
+    
+        }
+     }
+    </script>
+    
+    <style scoped>
+    .background{
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+        position: absolute;
+        display: flex;
+    }
+    .user{
+        width: 300px;
+    }
+    .pass{
+        width: 300px;
+    }
+
+    .login{
+        width: 500px;
+        height: 200px;
+        opacity: 0.60;
+        margin: auto;
+    }
+    </style>
